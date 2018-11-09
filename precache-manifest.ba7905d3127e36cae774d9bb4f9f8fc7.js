57ed13e7ffd5fcc9665366116340306dbf7be8a1
Eliezer Martins <eliezermarttin@gmail.com>
2018-11-09 09:50:17 -0200

    Updates

---
+++ precache-manifest.ba7905d3127e36cae774d9bb4f9f8fc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bde2a6d130abc251ae74292ae431e3e",
+    "url": "/Goweek-frontend/static/media/twitter.7bde2a6d.svg"
+  },
+  {
+    "revision": "c71630b0a771abe9b829bceaecacdcba",
+    "url": "/Goweek-frontend/static/media/like.c71630b0.svg"
+  },
+  {
+    "revision": "8b205e3538dc93d72340",
+    "url": "/Goweek-frontend/static/js/runtime~main.8b205e35.js"
+  },
+  {
+    "revision": "45f3b5083488f7953e95",
+    "url": "/Goweek-frontend/static/js/main.45f3b508.chunk.js"
+  },
+  {
+    "revision": "85f5c8bc9e97911cf0b5",
+    "url": "/Goweek-frontend/static/js/1.85f5c8bc.chunk.js"
+  },
+  {
+    "revision": "45f3b5083488f7953e95",
+    "url": "/Goweek-frontend/static/css/main.ad635e65.chunk.css"
+  },
+  {
+    "revision": "e48f608f2c6c1c755e96c699a51583b7",
+    "url": "/Goweek-frontend/index.html"
+  }
+];
